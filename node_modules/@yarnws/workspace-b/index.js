a5b8a10a4f8917f8ce28b72ecc9b8284439b4413
Rahul Kurumkar <rkurumkar@rocketsoftware.com>
2019-07-04 15:13:58 +0530

    first commit

---
+++ node_modules/@yarnws/workspace-b/index.js
@@ -0,0 +1,3 @@
+const {addNumber}  = require("@yarnws/workspace-a");
+
+console.log(addNumber(2,6));
---
+++ packages/workspace-a/index.js
@@ -0,0 +1,5 @@
+const addNumber = (number1, number2) => {
+	return number1 + number2;
+};
+
+module.exports = { addNumber } ;
